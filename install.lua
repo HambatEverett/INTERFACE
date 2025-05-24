43c15adea36e07321f27b723872e3445dd235297
Everett K. <hambateverett@gmail.com>
2025-05-24 00:02:37 -0700

    Update install.lua

--- install.lua
+++ install.lua
@@ -2,3 +2,7 @@
 -- INTERFACE
 
 fs.makeDir("interface/")
+shell.run("wget https://raw.githubusercontent.com/HambatEverett/INTERFACE/refs/heads/main/test/test.lua test.lua")
+fs.move("test.lua","interface/")
+
+print("Done")

aa72349cc404697f10d010bf3bd4fb9eac7732cb
Everett K. <hambateverett@gmail.com>
2025-05-25 19:22:12 -0700

    Update install.lua

--- install.lua
+++ install.lua
@@ -3,7 +3,7 @@
 
 if fs.exists("interface/vercont.lua") == false then
   fs.makeDir("interface/")
-  shell.run("wget https://raw.githubusercontent.com/HambatEverett/INTERFACE/refs/heads/main/interr/vercont.lua vercont.lua")
+  shell.run("wget https://raw.githubusercontent.com/HambatEverett/INTERFACE/refs/heads/main/inter/vercont.lua vercont.lua")
   fs.move("vercont.lua","interface/vercont.lua")
 else
   fs.delete("interface/vercont.lua")
